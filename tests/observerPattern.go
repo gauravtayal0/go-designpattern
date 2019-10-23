6526829b9edfbd687e428b62587bddd42585c7c4
Gaurav Tayal <gauravtayal0@gmail.com>
2019-10-23 16:57:16 +0530

    dont need fmt in main file

--- tests/observerPattern.go
+++ tests/observerPattern.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gauravtayal0/Go-DesignPattern/Observer"
 )
 
 func main() {
 	weatherSubject := Observer.NewSubject()
-	fmt.Println("hello", weatherSubject)
 
 	for i := 0; i < 10; i++ {
 		reporter1 := Observer.NewReporter(i)
